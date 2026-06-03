376b3e262c792b4bc4c4213d44554ac7f8ff7077
Rowan <48519289+scar055@users.noreply.github.com>
2026-06-03 12:29:52 +0200

    add api links to server js #4

--- server.js
+++ server.js
@@ -13,8 +13,25 @@ app.engine("liquid", engine.express());
 
 app.set("views", "./views");
 
+const golfers = "https://fdnd-agency.directus.app/items/into_golf_golfers";
+
+const rounds = "https://fdnd-agency.directus.app/items/into_golf_rounds";
+
+const handicapHistory =
+  "https://fdnd-agency.directus.app/items/into_golf_handicap_history";
+
+const milestones =
+  "https://fdnd-agency.directus.app/items/into_golf_milestones";
+
+const monthlyRanking =
+  "https://fdnd-agency.directus.app/items/into_golf_monthly_ranking";
+
 app.get("/", async function (request, response) {
-  response.render("index.liquid");
+  const golfer = await fetch(golfers + "/1");
+
+  const golferJson = await golfer.json();
+
+  response.render("index.liquid", {golfer: golferJson.data});
 });
 
 app.set("port", process.env.PORT || 8000);
